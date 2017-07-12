ebe409f65e65bba7fa800131ddbc1f3f7d453852
chenzhe07 <chenzhe07@gmail.com>
2017-07-12 08:07:51 +0000

    fix build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@ echo "building ..."
 rm -rf ./dbweb
 mkdir ./dbweb
 GOOS=linux GOARCH=amd64 go build -o ./dbweb/dbweb
-cp -r ./static ./dbweb/
+cp usercfg.conf ./dbweb/
+cp -r ./options ./dbweb/
+cp -r ./public ./dbweb/
 cp -r ./templates ./dbweb/
-cp -r ./langs ./dbweb/
 cp ./*.pem ./dbweb/
 tar zcvf ./dbweb.tar.gz ./dbweb
 echo "done."
